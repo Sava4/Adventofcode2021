077e02d186f4752a23552ba5320d0c783f3d8372
Alexander Savchenko <sasha.savchenko@gmail.com>
2021-12-09 15:11:33 +0200

    General cleanup and new method for detecting edges and top/bottom/left/right corners.

--- 21advent9_2.js
+++ 21advent9_2.js
@@ -11,63 +11,30 @@ const input = `2199943210
 9899965678`;
 
 const puzzle = input.split("\n").map(str => str.split("").map(n => Number(n))); //
-const vertical = puzzle.length - 1;
-const horizontal = puzzle[0].length - 1;
+const verticalIdx = puzzle.length - 1;
+const horizontalIdx = puzzle[0].length - 1;
 
 const lowPoints = [];
 
-for (let y = 0; y <= vertical; y++) {
-  for (let x = 0; x <= horizontal; x++) {
-    const searchCoords = [];
+for (let y = 0; y <= verticalIdx; y++) {
+  for (let x = 0; x <= horizontalIdx; x++) {
+    const searchCoords = [
+      [y, x - 1],
+      [y, x + 1],
+      [y + 1, x],
+      [y - 1, x]
+    ];
     const searchNumbers = [];
     const mainNumber = puzzle[y][x];
-
-    // top left corner
-    if (x === 0 && y === 0) {
-      searchCoords.push([0, 1], [1, 0]);
-    }
-    // first row
-    if (y === 0 && x > 0 && x < horizontal) {
-      searchCoords.push([0, x - 1], [0, x + 1], [1, x]);
-    }
-    // first column
-    if (x === 0 && y > 0 && y < vertical) {
-      searchCoords.push([y, x + 1], [y - 1, x], [y + 1, x]);
-    }
-    // middle of the square
-    if (x > 0 && x < horizontal && y > 0 && y < vertical) {
-      searchCoords.push([y, x - 1], [y, x + 1], [y - 1, x], [y + 1, x]);
-    }
-    // bottom left
-    if (x === 0 && y === vertical) {
-      searchCoords.push([y - 1, x], [y, 1]);
-    }
-    // bottom row
-    if (x > 0 && x < horizontal && y === vertical) {
-      searchCoords.push([y - 1, x], [y, x - 1], [y, x + 1]);
-    }
-    //last column
-    if (x === horizontal && y > 0 && y < vertical) {
-      searchCoords.push([y - 1, x], [y + 1, x], [y, x - 1]);
-    }
-    // bottom right corner
-    if (x === horizontal && y === vertical) {
-      searchCoords.push([vertical, horizontal - 1], [vertical - 1, horizontal]);
-    }
-
-    // top right corner
-    if (x === horizontal && y === 0) {
-      searchCoords.push([0, horizontal - 1], [1, x]);
-    }
-
     searchCoords.forEach(([y, x]) => {
-      const num = puzzle[y][x];
-      searchNumbers.push(num);
+      const num = puzzle?.[y]?.[x];
+      if (num !== undefined) {
+        searchNumbers.push(num);
+      }
     });
     if (searchNumbers.every(n => mainNumber < n)) {
       lowPoints.push([y, x, mainNumber]);
     }
-    // console.log(searchCoords)
   }
 }
 
@@ -84,59 +51,26 @@ lowPoints.forEach((lpt, idx) => {
   while (neighbors.length > 0) {
     let _neighbors = [];
 
-    neighbors.forEach(([y, x, val]) => {
-      //   console.log('Neib', y,x,val);
-      const searchCoords = [];
+    neighbors.forEach(([y, x, _]) => {
+      //   console.log('Neib', y,x,_);
+      const searchCoords = [
+        [y, x - 1],
+        [y, x + 1],
+        [y + 1, x],
+        [y - 1, x]
+      ];
 
-      // top left corner
-      if (x === 0 && y === 0) {
-        searchCoords.push([0, 1], [1, 0]);
-      }
-      // first row
-      if (y === 0 && x > 0 && x < horizontal) {
-        searchCoords.push([0, x - 1], [0, x + 1], [1, x]);
-      }
-      // first column
-      if (x === 0 && y > 0 && y < vertical) {
-        searchCoords.push([y, x + 1], [y - 1, x], [y + 1, x]);
-      }
-      // middle of the square
-      if (x > 0 && x < horizontal && y > 0 && y < vertical) {
-        searchCoords.push([y, x - 1], [y, x + 1], [y - 1, x], [y + 1, x]);
-      }
-      // bottom left
-      if (x === 0 && y === vertical) {
-        searchCoords.push([y - 1, x], [y, 1]);
-      }
-      // bottom row
-      if (x > 0 && x < horizontal && y === vertical) {
-        searchCoords.push([y - 1, x], [y, x - 1], [y, x + 1]);
-      }
-      //last column
-      if (x === horizontal && y > 0 && y < vertical) {
-        searchCoords.push([y - 1, x], [y + 1, x], [y, x - 1]);
-      }
-      // bottom right corner
-      if (x === horizontal && y === vertical) {
-        searchCoords.push(
-          [vertical, horizontal - 1],
-          [vertical - 1, horizontal]
-        );
-      }
-
-      // top right corner
-      if (x === horizontal && y === 0) {
-        searchCoords.push([0, horizontal - 1], [1, x]);
-      }
       const _searchCoords = searchCoords.filter(([yS, xS]) => {
         return !visitedCoords.some(([yV, xV]) => yV === yS && xV === xS);
       });
       _searchCoords.forEach(([y, x]) => {
-        const num = puzzle[y][x];
-        visitedCoords.push([y, x]);
-        if (num < 9) {
-          _neighbors.push([y, x, num]);
-          pointNeighbors.push([y, x, num]);
+        const num = puzzle?.[y]?.[x];
+        if (num !== undefined) {
+          visitedCoords.push([y, x]);
+          if (num < 9) {
+            _neighbors.push([y, x, num]);
+            pointNeighbors.push([y, x, num]);
+          }
         }
       });
     });
